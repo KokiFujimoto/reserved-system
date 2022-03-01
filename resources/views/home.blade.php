a67c96b2fe1d8512c18cd2695874815c76507d1f
Fujimoto <gegvp0330@gmail.com>
2022-03-01 16:40:42 +0900

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>簡易予約指標システム</title>
+</head>
+<body>
+    <header>
+        <h1>簡易予約指標システム</h1>
+    </header>
+    <main>
+        <section class="reserve-table">
+            <div class="nishimori d-flex">
+                <p>西森</p>
+                <table class="n-table table table-bordered">
+                    <tbody>
+                    <tr>
+                        <td class="n-1-1"></td>
+                        <td class="n-1-2"></td>
+                        <td class="n-1-3"></td>
+                        <td class="n-1-4"></td>
+                        <td class="n-1-5"></td>
+                        <td class="n-1-6"></td>
+                        <td class="n-1-7"></td>
+                        <td class="n-1-8"></td>
+                        <td class="n-1-9"></td>
+                        <td class="n-1-10"></td>
+                        <td class="n-1-11"></td>
+                        <td class="n-1-12"></td>
+                        <td class="n-1-13"></td>
+                        <td class="n-1-14"></td>
+                        <td class="n-1-15"></td>
+                        <td class="n-1-16"></td>
+                        <td class="n-1-17"></td>
+                        <td class="n-1-18"></td>
+                        <td class="n-1-19"></td>
+                        <td class="n-1-20"></td>
+                    </tr>
+                    <tr>
+                        <td class="n-2-1"></td>
+                        <td class="n-2-2"></td>
+                        <td class="n-2-3"></td>
+                        <td class="n-2-4"></td>
+                        <td class="n-2-5"></td>
+                        <td class="n-2-6"></td>
+                        <td class="n-2-7"></td>
+                        <td class="n-2-8"></td>
+                        <td class="n-2-9"></td>
+                        <td class="n-2-10"></td>
+                        <td class="n-2-11"></td>
+                        <td class="n-2-12"></td>
+                        <td class="n-2-13"></td>
+                        <td class="n-2-14"></td>
+                        <td class="n-2-15"></td>
+                        <td class="n-2-16"></td>
+                        <td class="n-2-17"></td>
+                        <td class="n-2-18"></td>
+                        <td class="n-2-19"></td>
+                        <td class="n-2-20"></td>
+                    </tr>
+                    <tr>
+                        <td class="n-3-1"></td>
+                        <td class="n-3-2"></td>
+                        <td class="n-3-3"></td>
+                        <td class="n-3-4"></td>
+                        <td class="n-3-5"></td>
+                        <td class="n-3-6"></td>
+                        <td class="n-3-7"></td>
+                        <td class="n-3-8"></td>
+                        <td class="n-3-9"></td>
+                        <td class="n-3-10"></td>
+                        <td class="n-3-11"></td>
+                        <td class="n-3-12"></td>
+                        <td class="n-3-13"></td>
+                        <td class="n-3-14"></td>
+                        <td class="n-3-15"></td>
+                        <td class="n-3-16"></td>
+                        <td class="n-3-17"></td>
+                        <td class="n-3-18"></td>
+                        <td class="n-3-19"></td>
+                        <td class="n-3-20"></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="master d-flex">
+                <p>マスター</p>
+                <table class="m-table table table-bordered">
+                    <tbody>
+                    <tr>
+                        <td class="m-1-1"></td>
+                        <td class="m-1-2"></td>
+                        <td class="m-1-3"></td>
+                        <td class="m-1-4"></td>
+                        <td class="m-1-5"></td>
+                        <td class="m-1-6"></td>
+                        <td class="m-1-7"></td>
+                        <td class="m-1-8"></td>
+                        <td class="m-1-9"></td>
+                        <td class="m-1-10"></td>
+                        <td class="m-1-11"></td>
+                        <td class="m-1-12"></td>
+                        <td class="m-1-13"></td>
+                        <td class="m-1-14"></td>
+                        <td class="m-1-15"></td>
+                        <td class="m-1-16"></td>
+                        <td class="m-1-17"></td>
+                        <td class="m-1-18"></td>
+                        <td class="m-1-19"></td>
+                        <td class="m-1-20"></td>
+                    </tr>
+                    <tr>
+                        <td class="m-2-1"></td>
+                        <td class="m-2-2"></td>
+                        <td class="m-2-3"></td>
+                        <td class="m-2-4"></td>
+                        <td class="m-2-5"></td>
+                        <td class="m-2-6"></td>
+                        <td class="m-2-7"></td>
+                        <td class="m-2-8"></td>
+                        <td class="m-2-9"></td>
+                        <td class="m-2-10"></td>
+                        <td class="m-2-11"></td>
+                        <td class="m-2-12"></td>
+                        <td class="m-2-13"></td>
+                        <td class="m-2-14"></td>
+                        <td class="m-2-15"></td>
+                        <td class="m-2-16"></td>
+                        <td class="m-2-17"></td>
+                        <td class="m-2-18"></td>
+                        <td class="m-2-19"></td>
+                        <td class="m-2-20"></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="lady d-flex">
+                <p>お姉さん</p>
+                <table class="l-table table table-bordered">
+                    <tbody>
+                        <tr>
+                            <td class="l-1-1"></td>
+                            <td class="l-1-2"></td>
+                            <td class="l-1-3"></td>
+                            <td class="l-1-4"></td>
+                            <td class="l-1-5"></td>
+                            <td class="l-1-6"></td>
+                            <td class="l-1-7"></td>
+                            <td class="l-1-8"></td>
+                            <td class="l-1-9"></td>
+                            <td class="l-1-10"></td>
+                            <td class="l-1-11"></td>
+                            <td class="l-1-12"></td>
+                            <td class="l-1-13"></td>
+                            <td class="l-1-14"></td>
+                            <td class="l-1-15"></td>
+                            <td class="l-1-16"></td>
+                            <td class="l-1-17"></td>
+                            <td class="l-1-18"></td>
+                            <td class="l-1-19"></td>
+                            <td class="l-1-20"></td>
+                        </tr>
+                        <tr>
+                            <td class="l-2-1"></td>
+                            <td class="l-2-2"></td>
+                            <td class="l-2-3"></td>
+                            <td class="l-2-4"></td>
+                            <td class="l-2-5"></td>
+                            <td class="l-2-6"></td>
+                            <td class="l-2-7"></td>
+                            <td class="l-2-8"></td>
+                            <td class="l-2-9"></td>
+                            <td class="l-2-10"></td>
+                            <td class="l-2-11"></td>
+                            <td class="l-2-12"></td>
+                            <td class="l-2-13"></td>
+                            <td class="l-2-14"></td>
+                            <td class="l-2-15"></td>
+                            <td class="l-2-16"></td>
+                            <td class="l-2-17"></td>
+                            <td class="l-2-18"></td>
+                            <td class="l-2-19"></td>
+                            <td class="l-2-20"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="assistant d-flex">
+                <p>ｱｼｽﾀﾝﾄ</p>
+                <table class="a-table table table-bordered">
+                    <tbody>
+                        <tr>
+                            <td class="a-1-1"></td>
+                            <td class="a-1-2"></td>
+                            <td class="a-1-3"></td>
+                            <td class="a-1-4"></td>
+                            <td class="a-1-5"></td>
+                            <td class="a-1-6"></td>
+                            <td class="a-1-7"></td>
+                            <td class="a-1-8"></td>
+                            <td class="a-1-9"></td>
+                            <td class="a-1-10"></td>
+                            <td class="a-1-11"></td>
+                            <td class="a-1-12"></td>
+                            <td class="a-1-13"></td>
+                            <td class="a-1-14"></td>
+                            <td class="a-1-15"></td>
+                            <td class="a-1-16"></td>
+                            <td class="a-1-17"></td>
+                            <td class="a-1-18"></td>
+                            <td class="a-1-19"></td>
+                            <td class="a-1-20"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="content">
+                <div class="reserve-time d-flex flex-row">
+                    <p class="time-1">9:00</p>
+                    <p class="time-2">9:30</p>
+                    <p class="time-3">10:00</p>
+                    <p class="time-4">10:30</p>
+                    <p class="time-5">11:00</p>
+                    <p class="time-6">11:30</p>
+                    <p class="time-7">12:00</p>
+                    <p class="time-8">12:30</p>
+                    <p class="time-9">13:00</p>
+                    <p class="time-10">13:30</p>
+                    <p class="time-11">14:00</p>
+                    <p class="time-12">14:30</p>
+                    <p class="time-13">15:00</p>
+                    <p class="time-14">15:30</p>
+                    <p class="time-15">16:00</p>
+                    <p class="time-16">16:30</p>
+                    <p class="time-17">17:00</p>
+                    <p class="time-18">17:30</p>
+                    <p class="time-19">18:00</p>
+                    <p class="time-20">18:30</p>
+                    <p class="time-21">19:00</p>
+                </div>
+            </div>
+        </section>
+        <section class="reserve d-flex">
+            <p>予約指標</p>
+            <table class="r-table table table-bordered">
+                <tbody>
+                    <tr>
+                        <td class="r-1-1"></td>
+                        <td class="r-1-2"></td>
+                        <td class="r-1-3"></td>
+                        <td class="r-1-4"></td>
+                        <td class="r-1-5"></td>
+                        <td class="r-1-6"></td>
+                        <td class="r-1-7"></td>
+                        <td class="r-1-8"></td>
+                        <td class="r-1-9"></td>
+                        <td class="r-1-10"></td>
+                        <td class="r-1-11"></td>
+                        <td class="r-1-12"></td>
+                        <td class="r-1-13"></td>
+                        <td class="r-1-14"></td>
+                        <td class="r-1-15"></td>
+                        <td class="r-1-16"></td>
+                        <td class="r-1-17"></td>
+                        <td class="r-1-18"></td>
+                        <td class="r-1-19"></td>
+                        <td class="r-1-20"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+        <section class="reserve-button">
+            <div class="register">
+                
+            </div>
+        </section>
+    </main>
+</body>
+</html>
